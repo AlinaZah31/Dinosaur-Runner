37f5b38c3a73071846c6124c1c917e1c4d7e2389
AlinaZah31 <alina_shuvi@yahoo.com>
2024-10-25 16:32:52 +0200

    Update script.js (add clearInterval)

--- script.js
+++ script.js
@@ -1,61 +1,69 @@
+const MAX_BOARD_LINES = 5, MAX_BOARD_COLS = 40, HUNDRED = 100, ONE_SECOND = 1000;
 let time = 0;
-const boardLines = 5, boardCols = 40, HUNDRED = 100, oneSecond = 1000;
-let lineDino = boardLines - 1, colDino = 2;
-let colObject = [boardCols - 1], lineObj = boardLines - 1;
-let dinoGoesDown = 0, noObjects = 0;
-
-generateButtons();
+let lineDino = MAX_BOARD_LINES - 1;
+let colDino = 2;
+let colObject = [MAX_BOARD_COLS - 1];
+let lineObj = MAX_BOARD_LINES - 1;
+let dinoGoesDown = 0;
+let noObjects = 0;
+let movingUp, movingDown;
 
 function generateButtons() {
-    for (let i = 0; i < boardLines; ++i) {
-        for (let j = 0; j < boardCols; ++j) {
+    for (let i = 0; i < MAX_BOARD_LINES; ++i) {
+        for (let j = 0; j < MAX_BOARD_COLS; ++j) {
             const button = document.createElement("button");
             button.type = "button";
             button.id = i * HUNDRED + j;
+            //button.id = i + "-" + j;
             button.innerText = '-';
             document.body.appendChild(button);    
-            if (i == boardLines - 1 && j == colDino) {
+            if (i === MAX_BOARD_LINES - 1 && j === colDino) {
                 button.style.backgroundColor = "green";
             }
-            if (i == boardLines - 1 && j == colObject[noObjects]) { 
+            if (i === MAX_BOARD_LINES - 1 && j === colObject[noObjects]) { 
                 button.style.backgroundColor = "red";
             }
-            if (j == boardCols - 1 ) {
+            if (j === MAX_BOARD_COLS - 1 ) {
                 document.body.appendChild(document.createElement("br"));
             }
         }  
     } 
 }
 
+generateButtons();
+
 function startStopWatch() {
     document.getElementById('Score-text').innerHTML = '0';
-    setInterval(increaseTimeAndAddMoreObjects, oneSecond);
-    setInterval(moveObjectsLeft, oneSecond / 10);
+    setInterval(increaseTimeAndAddMoreObjects, ONE_SECOND);
+    setInterval(moveObjectsLeft, ONE_SECOND / 10);
 }
 
 function increaseTimeAndAddMoreObjects() {
     for (let i = 0; i <= noObjects; ++i) {
-        if (lineDino == lineObj && colDino == colObject[i]) {
-            return time;
+        if (lineDino === lineObj && colDino === colObject[i]) {
+            //return time;
+            return;
         }
     }
     ++time;
-    if (time % 2 == 0) {
-        colObject[++noObjects] = boardCols - 1;
+    if (time % 2 === 0) {
+        colObject[++noObjects] = MAX_BOARD_COLS - 1;
     }
     document.getElementById('Score-text').innerHTML = time;
 }
 
 function moveObjectsLeft() {
     for (let i = 0; i <= noObjects; ++i) {
-        if (lineDino == lineObj && colDino == colObject[i]) {
+        if (lineDino === lineObj && colDino === colObject[i]) {
             return;
         }
         if (colObject[i] >= 0) {
-            const strangeObject = document.getElementById(lineObj * HUNDRED + colObject[i]);
+            const strangeObject = document.getElementById(lineObj * HUNDRED
+                + colObject[i]);
             strangeObject.style.backgroundColor = "red";
-            if (colObject[i] != boardCols - 1) {
-                const prevObject = document.getElementById(lineObj * HUNDRED + colObject[i] + 1);
+            if (colObject[i] != MAX_BOARD_COLS - 1) {
+                const prevObject = document.getElementById(lineObj * HUNDRED 
+                    + colObject[i] + 1);
                 prevObject.style.backgroundColor = "gray";
             }
         }
@@ -63,46 +71,47 @@ function moveObjectsLeft() {
     }
 }
 
-dinosaurJumps();
-
 function dinosaurJumps() {
     const input = document.querySelector("input");
-    const log = document.getElementById("log");
     input.addEventListener("keydown", checkKey);
 
     function checkKey(e) {
         textContent = e.code;
-        if (textContent == "ArrowUp") {
+        if (textContent === "ArrowUp") {
             dinoGoesDown = 0;
-            setInterval(dinoMovesUp, oneSecond / 10);
+            movingUp = setInterval(dinoMovesUp, ONE_SECOND / 10);
         }
     }
 
     function dinoMovesUp() {
-        if (dinoGoesDown == 0) {
+        if (dinoGoesDown === 0) {
             const dinosaur = document.getElementById((lineDino - 1) * HUNDRED + colDino);
             dinosaur.style.backgroundColor = "green";
             const prevDino = document.getElementById(lineDino * HUNDRED + colDino);
             prevDino.style.backgroundColor = "gray"; 
             --lineDino;  
         }  
-        if (lineDino == 0) {
+        if (lineDino === 0) {
+            clearInterval(movingUp);
             dinoGoesDown = 1;
-            setInterval(dinoMovesDown, oneSecond / 10);
+            movingDown = setInterval(dinoMovesDown, ONE_SECOND / 10);
         }  
     }
      
     function dinoMovesDown() {
-        if (dinoGoesDown == 1) {
+        if (dinoGoesDown === 1) {
             const dinosaur = document.getElementById((lineDino + 1) * HUNDRED + colDino);
             dinosaur.style.backgroundColor = "green";
             const prevDino = document.getElementById(lineDino * HUNDRED + colDino);
             prevDino.style.backgroundColor = "gray";
             ++lineDino;
         }
-        if (lineDino == boardLines - 1) {
-            dinoGoesDown = 2;
+        if (lineDino === MAX_BOARD_LINES - 1) {
+            clearInterval(movingDown);
+            dinoGoesDown = 0;
         }
     }
 }
+
+dinosaurJumps();
      
